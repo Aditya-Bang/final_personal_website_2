386fcccb0348762e49517b3a0cdc69774dcac91b
Aditya Bang <adityabang15@gmail.com>
2024-04-15 19:58:13 -0400

    linting issue fixed

--- app/about/page.js
+++ app/about/page.js
@@ -34,16 +34,16 @@ const About = () => {
           </div>
 
           <p className='text-xl'>
-            Hello and welcome to my personal website. I'm Aditya Bang, and I'm currently a second-year&nbsp;<span className='text-blue-400'>Co-op computer science</span>&nbsp;student at the&nbsp;<span className='text-blue-400'>University of Waterloo</span>.
+            Hello and welcome to my personal website. I&apos;m Aditya Bang, and I&apos;m currently a second-year&nbsp;<span className='text-blue-400'>Co-op computer science</span>&nbsp;student at the&nbsp;<span className='text-blue-400'>University of Waterloo</span>.
           </p>
           <p className='text-xl'>
-            My interests revolve around programming, mathematics, and physics. So far, I've mainly created full-stack projects, some of which involve AI. My curiosity extends to understanding the mathematical principles behind AI through reading research papers. Additionally, I'm keen on competitive programming, particularly in graph theory, and exploring pure mathematics in my classes at UWaterloo.
+            My interests revolve around programming, mathematics, and physics. So far, I&apos;ve mainly created full-stack projects, some of which involve AI. My curiosity extends to understanding the mathematical principles behind AI through reading research papers. Additionally, I&apos;m keen on competitive programming, particularly in graph theory, and exploring pure mathematics in my classes at UWaterloo.
           </p>
           <p className='text-xl'>
             Beyond academics, I enjoy swimming and got my Bronze Cross Assistant lifeguard certification in 2019. I also like watching basketball.
           </p>
           <p className='text-xl'>
-            Currently, I'm seeking internships for the Fall of 2024. Please don't hesitate to&nbsp;
+            Currently, I&apos;m seeking internships for the Fall of 2024. Please don&apos;t hesitate to&nbsp;
 
             <div className="relative group inline-flex">
               <span className="absolute left-0 bottom-0 w-full h-1 bg-blue-400 group-hover:h-full group-hover:transition-all"></span>
