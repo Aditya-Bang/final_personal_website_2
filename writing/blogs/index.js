112b2f9cae068465d7bb8ac21757ec665b966668
Aditya Bang <adityabang15@gmail.com>
2026-04-29 22:52:08 -0400

    add more parts to blog

--- writing/blogs/index.js
+++ writing/blogs/index.js
@@ -1,8 +1,12 @@
 import fusedSpmmGemmPart1 from './fused-spmm-gemm-part-1.json';
+import fusedSpmmGemmPart2 from './fused-spmm-gemm-part-2.json';
+import fusedSpmmGemmPart3 from './fused-spmm-gemm-part-3.json';
 
 
 const blogs = [
     fusedSpmmGemmPart1,
+    fusedSpmmGemmPart2,
+    fusedSpmmGemmPart3,
 ];
 
 export default blogs;
